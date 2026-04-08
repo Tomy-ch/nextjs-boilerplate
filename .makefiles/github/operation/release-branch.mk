6c0b5b3b3b42401a571ddc3758155b0f506cdd17
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-04-08 19:43:13 +0900

    Fix: branch-minorとbranch-majorの説明を修正

--- .makefiles/github/operation/release-branch.mk
+++ .makefiles/github/operation/release-branch.mk
@@ -28,8 +28,8 @@ endef
 
 .PHONY: hotfix-patch ## hotfixブランチ(vX.Y.Z+1)を作成して、デフォルトブランチに設定(現在のタグ基準)
 .PHONY: branch-patch ## releaseブランチ(vX.Y.Z+1)を作成して、デフォルトブランチに設定(現在のタグ基準)
-.PHONY: branch-minor ## releaseブランチ(vX.Y+1.Z)を作成して、デフォルトブランチに設定(現在のタグ基準)
-.PHONY: branch-major ## releaseブランチ(vX+1.Y.Z)を作成して、デフォルトブランチに設定(現在のタグ基準)
+.PHONY: branch-minor ## releaseブランチ(vX.Y+1.0)を作成して、デフォルトブランチに設定(現在のタグ基準)
+.PHONY: branch-major ## releaseブランチ(vX+1.0.0)を作成して、デフォルトブランチに設定(現在のタグ基準)
 
 hotfix-patch:
 	@V=$(call get-latest-version); \
