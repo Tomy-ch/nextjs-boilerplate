e1df8d358f2ca918d23a1631e0f3d224c6634553
Tomy-ch <toyo.chika.fuku@gmail.com>
2026-04-08 19:39:43 +0900

    Refactor: activate-toolsターゲットを削除し、install-toolsに統合

--- .makefiles/tools/setup.mk
+++ .makefiles/tools/setup.mk
@@ -1,13 +1,9 @@
 ## ツール関連
-.PHONY: activate-tools ## 開発に必要なツールを有効化する
 .PHONY: install-tools ## 開発に必要なツールをインストールする
-
-activate-tools: install-tools
-	@echo "🔄 Activating tools..."
-	@corepack enable
-	@echo "✅ Tools activated successfully."
 
 install-tools:
 	@echo "🔄 Installing tools..."
-	COREPACK_ENABLE_DOWNLOAD_PROMPT=0 pnpm -v
+	@corepack enable
+	@COREPACK_ENABLE_DOWNLOAD_PROMPT=0 corepack prepare pnpm@latest --activate
+	@pnpm -v
 	@echo "✅ Tools installed successfully."
